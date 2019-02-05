2a98ccb761f4258f96c7bf06677dd8ee1aadd14d
Sergey <xxsmotur@gmail.com>
2019-02-05 21:54:53 +0500

    Add example for make users

--- examples/basic.py
+++ examples/basic.py
@@ -1,1 +1,11 @@
- self._cursor.execute("CREATE TABLE predicto(id SERIAL PRIMARY KEY, name TEXT NOT NULL, created TIMESTAMP);")
+import psycopg2
+from datetime import datetime
+
+def make_users(count=100):
+    connect = psycopg2.connect(database='predicto', host='localhost', port=5432, user='predicto', password='predicto')
+    cursor = connect.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS predicto(id SERIAL PRIMARY KEY, name TEXT NOT NULL, created TIMESTAMP);")
+    cursor.execute('INSERT INTO predicto(name, created) VALUES (%s, %s)', ('user', datetime.now()))
+    connect.close()
+
+make_users()
