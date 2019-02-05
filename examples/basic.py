c317196cb3e563065d52f28c06f1785303e2fbf8
Sergey <xxsmotur@gmail.com>
2019-02-05 22:39:46 +0500

    generation of users

--- examples/basic.py
+++ examples/basic.py
@@ -1,11 +1,15 @@
 import psycopg2
-from datetime import datetime
+from datetime import datetime, timedelta
+import random 
+import string
 
 def make_users(count=100):
     connect = psycopg2.connect(database='predicto', host='localhost', port=5432, user='predicto', password='predicto')
     cursor = connect.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS predicto(id SERIAL PRIMARY KEY, name TEXT NOT NULL, created TIMESTAMP);")
-    cursor.execute('INSERT INTO predicto(name, created) VALUES (%s, %s)', ('user', datetime.now()))
+    for i in range(count):
+        user = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+        cursor.execute('INSERT INTO predicto(name, created) VALUES (%s, %s)', (user, datetime.now() - timedelta(hours=random.randint(1,30))))
     connect.close()
 
 make_users()
