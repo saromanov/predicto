224b02d48ec8a9e47efc3d46b0c18dd83646f6ce
Sergey <xxsmotur@gmail.com>
2019-02-05 20:54:10 +0500

    move predict method under Predicto class

--- predicto.py
+++ predicto.py
@@ -20,18 +20,18 @@ class Predicto:
         labels = [self._datetimeCol]
         labels.extend(labels)
         df = self._make_dataframe(X, colums=labels)
+        return self._predict(df)
     
     def _make_dataframe(data, columns=['datetime', 'items']):
         """returns pandas dataframe
         """
         return pd.DataFrame(data, columns=columns)
-
-
-def predict(data):
-    """ predicting of the future data points
-    based on exist data
-    """
-    p = Prophet()
-    p.fit(data)
-    future = p.make_future_dataframe(periods=366)
-    return p.predict(future)
+    
+    def _predict(data):
+        """ predicting of the future data points
+        based on exist data
+        """
+        p = Prophet()
+        p.fit(data)
+        future = p.make_future_dataframe(periods=366)
+        return p.predict(future)
